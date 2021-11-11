c9034e91a39f8f3a2a3d5c51e53c5e5075462370
SthefanyLara <sthefany.gabriele@hotmail.com>
2021-11-11 20:31:36 -0300

    desenvolvendo LoginPage

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Router from "./components/Router";
+import LoginPage from "./components/LoginPage";
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage/>
+    </div>
+
+  );
+
+}
+
+export default App;
